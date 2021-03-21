c8e133cf7337add66127ef7834a00333d66451d4
muralikrishna188 <muralid188@outlook.com>
2021-03-21 08:51:37 +0530

    updates

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
       resource_group_name = "backendazure"
       storage_account_name = "azurermstorage12"
-      container_name = "containertfstate"
+      container_name = "tfcicd"
       key            = "azure.containertfstate"
       access_key     = "nYxE3hirB282Jo1BKJbxmOteUP2dS5HkaIoNsmteCcsaUFnl4nqBMKA4E7DM19ckY8SliFIHXQ0jkIR33HO2fA=="
   }
